9919d287b3e24711ddbdf76f21b3c9846152f08f
SonamBeginning <201551079@iiitvadodara.ac.in>
2016-11-28 21:24:20 +0530

    Day 0 code

---
+++ Hello World/src/Sonam/Solution.java
@@ -0,0 +1,10 @@
+package Sonam;
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String inputString=sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+	}
+}
